446454993a76a00b4f9904a0a2a3713248cd8277
Alija Hodzic <hodzic.alija5@gmail.com>
2022-02-02 20:58:05 +0100

    data will now be fetched from corresponding components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,7 @@
-import { useEffect, useMemo } from "react";
-import { useDispatch } from "react-redux";
 import MainScreen from "./components/home/MainScreen";
 import Navbar from "./components/navbar/Navbar";
-import { getMovies } from "./store/moviesSlice";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useMemo(() => {
-    dispatch(getMovies("/api/movies", "GET", {}));
-  }, []);
-
   return (
     <div className="App">
       <Navbar />
